cd54635a8a547f68e914987e924e10df501dae74
Matteo Favaro <matteo.favaro@hexagon.com>
2021-11-16 16:07:10 +0100

    refine usage

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,7 @@ RUN apk --no-cache add \
     libressl-dev \
     mailcap \
     fuse-dev \
+    sudo \
     curl-dev && \
   git clone https://github.com/s3fs-fuse/s3fs-fuse.git && \
   cd s3fs-fuse && \
@@ -23,17 +24,9 @@ RUN apk --no-cache add \
   make -j && \
   make install
 
-FROM alpine
+FROM build
 
 # Metadata
-LABEL MAINTAINER efrecon+github@gmail.com
-LABEL org.opencontainers.image.title="efrecon/s3fs"
-LABEL org.opencontainers.image.description="Mount S3 buckets from within a container and expose them to host/containers"
-LABEL org.opencontainers.image.authors="Emmanuel Frécon <efrecon+github@gmail.com>"
-LABEL org.opencontainers.image.url="https://github.com/efrecon/docker-s3fs-client"
-LABEL org.opencontainers.image.documentation="https://github.com/efrecon/docker-s3fs-client/README.md"
-LABEL org.opencontainers.image.source="https://github.com/efrecon/docker-s3fs-client/Dockerfile"
-
 COPY --from=build /usr/bin/s3fs /usr/bin/s3fs
 
 # Specify URL and secrets. When using AWS_S3_SECRET_ACCESS_KEY_FILE, the secret
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,30 +1,46 @@
 #! /usr/bin/env sh
-
+set -x
 # Where are we going to mount the remote bucket resource in our container.
-DEST=${AWS_S3_MOUNT:-/opt/s3fs/bucket}
+export DEST=${AWS_S3_MOUNT:-/opt/s3fs/bucket}
 
 # Check variables and defaults
-if [ -z "${AWS_S3_ACCESS_KEY_ID}" -a -z "${AWS_S3_SECRET_ACCESS_KEY}" -a -z "${AWS_S3_SECRET_ACCESS_KEY_FILE}" -a -z "${AWS_S3_AUTHFILE}" ]; then
+if [ -z "${AWS_S3_ACCESS_KEY_ID}" \
+-a -z "${AWS_S3_SECRET_ACCESS_KEY}" \ 
+-a -z "${AWS_S3_SECRET_ACCESS_KEY_FILE}" \
+-a -z "${AWS_S3_AUTHFILE}" \
+-a -z "${AWS_S3_SESSION_TOKEN}" \
+-a -z "${AWS_IAM_ROLE}" \
+]; then
     echo "You need to provide some credentials!!"
     exit
 fi
+
+export AWSACCESSKEYID=${AWS_S3_ACCESS_KEY_ID}
+export AWSSECRETACCESSKEY=${AWS_S3_SECRET_ACCESS_KEY}
+export AWSSESSIONTOKEN=${AWS_S3_SESSION_TOKEN}
+
+
 if [ -z "${AWS_S3_BUCKET}" ]; then
     echo "No bucket name provided!"
     exit
 fi
 if [ -z "${AWS_S3_URL}" ]; then
-    AWS_S3_URL="https://s3.amazonaws.com"
+    export AWS_S3_URL="https://s3.amazonaws.com"
 fi
 
 if [ -n "${AWS_S3_SECRET_ACCESS_KEY_FILE}" ]; then
-    AWS_S3_SECRET_ACCESS_KEY=$(read ${AWS_S3_SECRET_ACCESS_KEY_FILE})
+    export AWS_S3_SECRET_ACCESS_KEY=$(read ${AWS_S3_SECRET_ACCESS_KEY_FILE})
 fi
 
 # Create or use authorisation file
 if [ -z "${AWS_S3_AUTHFILE}" ]; then
-    AWS_S3_AUTHFILE=/opt/s3fs/passwd-s3fs
+    export AWS_S3_AUTHFILE=/opt/s3fs/passwd-s3fs
     echo "${AWS_S3_ACCESS_KEY_ID}:${AWS_S3_SECRET_ACCESS_KEY}" > ${AWS_S3_AUTHFILE}
     chmod 600 ${AWS_S3_AUTHFILE}
+fi
+
+if [ ${S3FS_DEBUG} = "1" ]; then
+    cat ${AWS_S3_AUTHFILE}
 fi
 
 # forget about the password once done (this will have proper effects when the
@@ -34,29 +50,34 @@ if [ -n "${AWS_S3_SECRET_ACCESS_KEY}" ]; then
 fi
 
 # Create destination directory if it does not exist.
-if [ ! -d ${DEST} ]; then
-    mkdir -p ${DEST}
-fi
+mkdir -p ${DEST}
 
-GROUP_NAME=$(getent group "${GID}" | cut -d":" -f1)
+export GROUP_NAME=$(getent group "${GID}" | cut -d":" -f1)
 
 # Add a group
 if [ $GID -gt 0 -a -z "${GROUP_NAME}" ]; then
-    addgroup -g $GID -S $GID
-    GROUP_NAME=$GID
+    addgroup -g ${GID} -S ${GID}
+    export GROUP_NAME=${GID}
 fi
 
 # Add a user
 if [ ${UID} -gt 0 ]; then
-    adduser -u $UID -D -G $GROUP_NAME $UID
-    RUN_AS=${UID}
-    chown ${UID}:${GID} ${AWS_S3_MOUNT}
+    adduser -u ${UID} -D -G ${GROUP_NAME} ${UID}
+    export RUN_AS=${UID}
+    chown ${UID}:${GID} ${DEST}
     chown ${UID}:${GID} ${AWS_S3_AUTHFILE}
     chown ${UID}:${GID} /opt/s3fs
+
 fi
 
+echo "Testing mounting dir"
+ls -la ${DEST}/../
+
+ls -la ${DEST}
+
+
 # Debug options
-DEBUG_OPTS=
+export DEBUG_OPTS=
 if [ ${S3FS_DEBUG} = "1" ]; then
     export DEBUG_OPTS="-d -d"
 fi
@@ -69,31 +90,52 @@ fi
 # Mount and verify that something is present. davfs2 always creates a lost+found
 # sub-directory, so we can use the presence of some file/dir as a marker to
 # detect that mounting was a success. Execute the command on success.
-if [ -n "${AWS_IAM_ROLE}" ]; then
+if [ -z "${AWS_IAM_ROLE}" -a -z "${AWS_S3_SESSION_TOKEN}" ]; then
+    echo "Using Credential"
 
-    su - ${RUN_AS} -c "s3fs ${DEBUG_OPTS} ${S3FS_ARGS} \
+    sudo -u ${RUN_AS} -E s3fs ${DEBUG_OPTS} ${S3FS_ARGS} \
         -o passwd_file=${AWS_S3_AUTHFILE} \
         -o url=${AWS_S3_URL} \
         -o uid=${UID} \
         -o gid=${GID} \
-        ${AWS_S3_BUCKET} ${AWS_S3_MOUNT}"
-else 
+        ${AWS_S3_BUCKET} ${DEST}
 
-    su - ${RUN_AS} -c "s3fs ${DEBUG_OPTS} ${S3FS_ARGS} \
+elif [ -z "${AWS_IAM_ROLE}" -a -n "${AWS_S3_SESSION_TOKEN}" ]; then
+    echo "Using session Token"
+    echo "RUN AS: ${RUN_AS}"
+
+    sudo -u ${RUN_AS} -E s3fs -o retries=20 \
+    -o uid=${UID} \
+    -o gid=${GID} \
+    ${AWS_S3_BUCKET} ${DEST}
+
+elif [ -n "${AWS_IAM_ROLE}" -a -z "${AWS_S3_SESSION_TOKEN}" ]; then
+    echo "Using IAM ROLE"
+    sudo -u ${RUN_AS} -E s3fs ${DEBUG_OPTS} ${S3FS_ARGS} \
         -o iam_role=${AWS_IAM_ROLE} \
         -o url=${AWS_S3_URL} \
         -o uid=${UID} \
         -o gid=${GID} \
-        ${AWS_S3_BUCKET} ${AWS_S3_MOUNT}"
+        ${AWS_S3_BUCKET} ${AWS_S3_MOUNT}
 
+else 
+    echo "MOUNT FAILURE no credentials"
+    exit 1
 fi
 # s3fs can claim to have a mount even though it didn't succeed.
 # Doing an operation actually forces it to detect that and remove the mount.
-ls "${AWS_S3_MOUNT}"
+ls -la "${DEST}"
 
-mounted=$(mount | grep fuse.s3fs | grep "${AWS_S3_MOUNT}")
+ls -la "${DEST}/../"
+
+sudo -u ${RUN_AS} ls -la "${DEST}/../"
+sudo -u ${RUN_AS} ls -la "${DEST}"
+
+mounted=$(mount | grep fuse.s3fs | grep "${DEST}")
 if [ -n "${mounted}" ]; then
-    echo "Mounted bucket ${AWS_S3_BUCKET} onto ${AWS_S3_MOUNT}"
+
+    echo "Mount line: ${mounted}"
+    echo "Mounted bucket ${AWS_S3_BUCKET} onto ${DEST}"
     exec "$@"
 else
     echo "Mount failure"
